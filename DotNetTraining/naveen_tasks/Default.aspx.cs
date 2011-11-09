84e644b9bbeefd55354d92f5eeaad737174f02e3
naveen.raju36 <naveen.raju36@fd1cb45f-5239-55ee-a5f8-e7f836e0279e>
2011-11-09 04:53:36 +0000

    Added Google and Yahoo Pages

--- DotNetTraining/naveen_tasks/Default.aspx.cs
+++ DotNetTraining/naveen_tasks/Default.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace YAHOOASP.NET
+namespace naveen_tasks
 {
     public partial class _Default : System.Web.UI.Page
     {
