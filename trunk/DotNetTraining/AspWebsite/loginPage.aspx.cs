ea3a58fc2296bb86568b792e660e752a1375d37b
satishgv1985 <satishgv1985@fd1cb45f-5239-55ee-a5f8-e7f836e0279e>
2011-11-11 06:09:44 +0000

    used ddlpostback

--- trunk/DotNetTraining/AspWebsite/loginPage.aspx.cs
+++ trunk/DotNetTraining/AspWebsite/loginPage.aspx.cs
@@ -9,7 +9,20 @@ public partial class loginPage : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        lblTime.Text = DateTime.Now.ToString();
+        if (IsPostBack)
+        {
+            lblTime.Text = "PostBack Req New time:" + DateTime.Now.ToString();
+            txtUserName.Text = "";
+
+        }
+        else
+        {
+            ddlState.Items.Add(new ListItem("-Select One-", "0"));
+            ddlState.Items.Add(new ListItem("Karnataka", "1"));
+            ddlState.Items.Add(new ListItem("Andhra", "2"));
+
+            lblTime.Text = DateTime.Now.ToString();
+        }
         //txtUserName.Text = "Page Load event";
     }
     protected void btnSignIn_Click(object sender, EventArgs e)
@@ -21,6 +34,35 @@ public partial class loginPage : System.Web.UI.Page
         strTemp = "un=" + userName;
         strTemp += "&pass" + password;
         strTemp += "&gen" + gender;
-        Response.Redirect("Welcome.aspx?un=" + userName + "&pass=" + password + "&gen=" + gender);
+        //Response.Redirect("Welcome.aspx?un=" + userName + "&pass=" + password + "&gen=" + gender);
+    }
+    protected void ddlState_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlState.SelectedValue == "0")
+        {
+            ddlDistrict.Items.Clear();
+            ddlDistrict.Items.Add(new ListItem("-Select State First-", "0"));
+        }
+
+        if (ddlState.SelectedValue == "1")
+        {
+            ddlDistrict.Items.Clear();
+            ListItem ltDistrict = new ListItem("Bangalore", "1");
+            ddlDistrict.Items.Add(ltDistrict);
+            ltDistrict = new ListItem("Mysore", "2");
+            ddlDistrict.Items.Add(ltDistrict);
+        }
+        if (ddlState.SelectedValue == "2")
+        {
+            ddlDistrict.Items.Clear();
+            ListItem ltDistrict = new ListItem("Hyderbad", "1");
+            ddlDistrict.Items.Add(ltDistrict);
+            ltDistrict = new ListItem("Kurnool", "2");
+            ddlDistrict.Items.Add(ltDistrict);
+        }
+    }
+    protected void ddlGender_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        pnlState.Visible = false;
     }
 }
