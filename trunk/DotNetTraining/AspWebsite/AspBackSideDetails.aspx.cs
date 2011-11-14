52a1d16cd836beda2b139da830a50a0337c63825
satishgv1985 <satishgv1985@fd1cb45f-5239-55ee-a5f8-e7f836e0279e>
2011-11-14 06:01:09 +0000

    added masterPages

---
+++ trunk/DotNetTraining/AspWebsite/AspBackSideDetails.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class AspBackSideDetails : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            int val = 3;
+            Label lblTextBoxes = new Label();
+            lblTextBoxes.ID = "lblTextsBoxes";
+            lblTextBoxes.Text = "No of Text Boxes: ";
+            pnlRuntime.Controls.Add(lblTextBoxes);
+
+            Table tbl = new Table();
+            tbl.ID = "tbl";
+            TableRow tr = new TableRow();
+            TableCell td;
+            RequiredFieldValidator rfvTBs;
+
+            TextBox tb;
+            for (int i = 0; i < val; i++)
+            {
+                tb = new TextBox();
+                tb.ID = "firstTextbox" + i;
+                rfvTBs = new RequiredFieldValidator();
+                rfvTBs.ID = "rfv_" + i;
+                rfvTBs.ControlToValidate = tb.ID;
+                rfvTBs.ErrorMessage = "Please enter for textbox "+i;
+
+                td = new TableCell();
+                td.CssClass = "tdClass";
+
+
+                td.Controls.Add(tb);
+                td.Controls.Add(rfvTBs);
+
+                tr.Cells.Add(td);
+
+            }
+            tbl.Rows.Add(tr);
+            pnlRuntime.Controls.Add(tbl);
+        }
+    }
+}
---
+++ trunk/DotNetTraining/AspWebsite/masterPages/News.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/masterPages/MasterPage.master" AutoEventWireup="true" CodeFile="News.aspx.cs" Inherits="masterPages_News" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<br />This is news section<br />
+</asp:Content>
+
