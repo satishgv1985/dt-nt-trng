d7a6bf095c334cb6905e76bf2a1738dd19116d91
satishgv1985 <satishgv1985@fd1cb45f-5239-55ee-a5f8-e7f836e0279e>
2011-11-21 05:41:50 +0000

    removed vb files, sessionLogin class

---
+++ trunk/DotNetTraining/AspWebsite/Session.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Session : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        if (!IsPostBack)
+        {
+            Session["name"] = "Asp.Net";
+            
+        }
+
+    }
+    protected void btnSession_Click(object sender, EventArgs e)
+    {
+        if (Session["name"] != null)
+            lblName.Text = (string)Session["name"];
+    }
+}
