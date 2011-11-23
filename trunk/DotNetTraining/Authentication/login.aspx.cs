563ca3ac1bb532efa05cfd7a6ba730bbc825a21d
amit.kant008 <amit.kant008@fd1cb45f-5239-55ee-a5f8-e7f836e0279e>
2011-11-23 06:06:02 +0000

    added forms Authentication project

---
+++ trunk/DotNetTraining/Authentication/login.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace Authentication
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSignIn_Click(object sender, EventArgs e)
+        {
+
+           // if (txtUserName.Text == "amit111" && txtPassword.Text == "aaa")
+           
+                FormsAuthentication.RedirectFromLoginPage(txtUserName.Text, true);
+            
+            //else
+                lblError.Text = "Invalid user name";
+        }
+    }
+}
