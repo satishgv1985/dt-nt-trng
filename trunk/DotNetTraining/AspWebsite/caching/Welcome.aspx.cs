2614396c6bf73ab9dcd4989cd1400a112fa855d3
amit.kant008 <amit.kant008@fd1cb45f-5239-55ee-a5f8-e7f836e0279e>
2011-11-25 06:18:54 +0000

    cache dependency class

---
+++ trunk/DotNetTraining/AspWebsite/caching/Welcome.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class caching_Welcome : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Cache["name"] != null)
+            lblCachedData.Text = Convert.ToString(Cache["name"]);
+        else
+            lblCachedData.Text = "Nothing cached";
+
+        Response.Write("hi");
+
+        Response.Write("--"+Cache["RemovedEmpoyees"]+"--");
+        
+    }
+}
---
+++ trunk/DotNetTraining/AspWebsite/caching/homePage.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Caching;
+using System.Xml;
+
+public partial class caching_homePage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Simple Cache
+        //Cache["name"] = "satish";
+        //Cache.Remove("name");
+
+        //File Based Dependency Cache
+        //CacheDependency cdFileBased = new CacheDependency(Server.MapPath("users.xml"));
+        //Cache.Insert("name", "satish", cdFileBased);
+
+        //time Based dependency -- absolute expiration
+        //Cache.Insert("name", "satish", null, DateTime.Now.AddSeconds(10), Cache.NoSlidingExpiration);
+
+        //time based dependency -- sliding expiration
+        //Cache.Insert("name", "satish", null, Cache.NoAbsoluteExpiration, TimeSpan.FromSeconds(15));
+     
+        //key based dependency
+        Cache["employee"] = "1";
+        //Create the array of cache key item names
+        string[] keys = new String[1];
+        keys[0] = "employee";
+        CacheDependency cdKey = new CacheDependency(null,keys);
+        CacheItemRemovedCallback circbEmployeeRemoved = new CacheItemRemovedCallback(employeeRemoved);
+        Cache.Insert("name", "satish",cdKey,Cache.NoAbsoluteExpiration,Cache.NoSlidingExpiration,CacheItemPriority.Default,circbEmployeeRemoved);
+        
+
+    }
+
+    protected void employeeRemoved(string str,object value, CacheItemRemovedReason reason)
+    {
+        if(reason==CacheItemRemovedReason.DependencyChanged)
+        Cache["RemovedEmpoyees"] = value;
+    }
+}
