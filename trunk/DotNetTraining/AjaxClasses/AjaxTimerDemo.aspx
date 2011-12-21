199e236b0572af2b76c4f0ca384d5fc63a45ba64
amit.kant008 <amit.kant008@fd1cb45f-5239-55ee-a5f8-e7f836e0279e>
2011-12-21 06:09:07 +0000

    AjaxSpinner Added

---
+++ trunk/DotNetTraining/AjaxClasses/AjaxTimerDemo.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AjaxTimerDemo.aspx.cs" Inherits="AjaxClasses.AjaxTimerDemo" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+       
+        <asp:Timer ID="Timer1" runat="server" Interval="5000" ontick="Timer1_Tick">
+        </asp:Timer>
+
+         <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+         <ContentTemplate>
+             <asp:Image ID="Image1" runat="server" ImageUrl="~/images/image1.jpg" 
+                 Height="300px" Width="300px" />
+         </ContentTemplate>
+         <Triggers>
+         <asp:AsyncPostBackTrigger ControlID="Timer1" />
+         </Triggers>
+        </asp:UpdatePanel>
+
+        <marquee>this is marque moving tag</marquee>
+    </div>
+    </form>
+</body>
+</html>
---
+++ trunk/DotNetTraining/AjaxClasses/UserNameAvailability.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AjaxClasses
+{
+    public partial class UserNameAvailability : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(2000);
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["AjaxConnectionString"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT emp_name FROM User_Info WHERE emp_name='" + txtUserName.Text + "'", con);
+            cmd.CommandType = CommandType.Text;
+           
+            SqlDataReader sdrUserName = cmd.ExecuteReader();
+
+            while (sdrUserName.Read())
+            {
+                lbMessage.Text = "Already Taken";
+               
+                lbMessage.CssClass = "taken";
+                
+                con.Close();
+                return;
+            }
+
+            lbMessage.Text = "UserName is Avaliable";
+        
+            lbMessage.CssClass = "available";
+            con.Close();
+
+        }
+    }
+}
