eda0f7da76eccdf56b5e901f48b2a87e6a34e07b
amit.kant008 <amit.kant008@fd1cb45f-5239-55ee-a5f8-e7f836e0279e>
2011-12-22 07:34:21 +0000

    Added Web Services

---
+++ trunk/DotNetTraining/AjaxClasses/WebServiceCustomer.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AjaxClasses
+{
+    public partial class WebServiceCustomer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetName_Click(object sender, EventArgs e)
+        {
+            localhost.MyCustomerService myCust = new localhost.MyCustomerService();
+            string name= myCust.GetCustomerNameById(Convert.ToInt32(txtId.Text));
+
+            lblName.Text = name;
+
+            lblName.Text += myCust.HelloWorld();
+ 
+        }
+    }
+}
